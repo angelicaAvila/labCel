b884ba7f37dbb81bcfc7dd954c371406ca680728
ANGELICA AVILA <angelica.avila@uabc.edu.mx>
2019-03-21 21:30:47 -0700

    top menu added to appModule

--- proyecto/src/app/app.module.ts
+++ proyecto/src/app/app.module.ts
@@ -4,11 +4,13 @@ import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { TopMenuComponent } from './menu/top-menu/top-menu.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    TopMenuComponent
   ],
   imports: [
     NgbModule,
