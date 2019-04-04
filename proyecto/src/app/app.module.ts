7b8e09b0416a90bc15856b5f544bb469af3dee40
ANGELICA AVILA <angelica.avila@uabc.edu.mx>
2019-04-03 19:30:20 -0700

    Display products: UI changes

--- proyecto/src/app/app.module.ts
+++ proyecto/src/app/app.module.ts
@@ -3,14 +3,18 @@ import { NgModule } from '@angular/core';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
+import { LoginComponent } from './menu/top-menu/login/login.component';
 import { TopMenuComponent } from './menu/top-menu/top-menu.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ProductsComponent } from './dashboard/products/products.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    TopMenuComponent
+    TopMenuComponent,
+    DashboardComponent,
+    ProductsComponent
   ],
   imports: [
     NgbModule,
