9606bf9ac81745c49a7bf6e5912f8d9f7287dbc5
angelicaAvila <angelica.avila@uabc.edu.mx>
2019-04-28 22:45:11 -0700

    Http module added

--- proyecto/src/app/shared/data.service.ts
+++ proyecto/src/app/shared/data.service.ts
@@ -4,7 +4,7 @@ import { Http } from '@angular/http'
 @Injectable()
 
 export class dataService{
-    constructor (private Http: http){}
+    constructor (){}
 
     storeData(){
        // this.http.post('https://labcel-e45e9.firebaseio.com/product.json');
