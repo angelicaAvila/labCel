79b1cd31d199df31abbdcea6fc0366b73f5f2781
Orlando Dante Lopez Moreno <dante.lopez@uabc.edu.mx>
2019-05-26 19:21:05 -0700

    Add files via upload

--- dashboard.component.ts
+++ dashboard.component.ts
@@ -11,7 +11,8 @@ export class DashboardComponent implements OnInit {
 
   myCategories: string;
   myBrands: string;
-
+  public isCollapsedCats =true;
+  public isCollapsedBrands =true;
   constructor() { }
 
   ngOnInit() {
@@ -20,17 +21,18 @@ export class DashboardComponent implements OnInit {
   showCategories(products : Product[]){
     var categories = [];
     this.myCategories ="<div>"
-    var n = 0;
+    // var n = 0;
     var cat = document.getElementById("cats");
     cat.innerHTML="";
     // while(cat.firstChild) cat.removeChild(cat.firstChild);
     for(var i = 0; i < products.length ; i++ ){
-      if(categories.indexOf(products[i].categoria) < 0 && n<=10){
+      if(categories.indexOf(products[i].categoria) < 0){
         categories.push(products[i].categoria);
         // console.log(i);
+        // var 
         this.myCategories += (`<button class ="btn-info" onClick="alert('${products[i].categoria}')"> ${products[i].categoria} </button><br>`);
         //(click) = "search("' + products[i].categoria + '")"
-        n++;
+        // n++;
       }
     }
     this.myCategories = this.myCategories + "</div>";
@@ -44,15 +46,15 @@ export class DashboardComponent implements OnInit {
     this.myBrands ="<div>";
     var brand = document.getElementById("brands");
     brand.innerHTML ="";
-    var n=0;
+    // var n=0;
 
     for(var i = 0; i < products.length ; i++ ){
-      if(brands.indexOf(products[i].marca) < 0 && n<=10){
+      if(brands.indexOf(products[i].marca) < 0){
         brands.push(products[i].marca);
         // console.log(i);
         this.myBrands += (`<button class ="btn-info"> ${products[i].marca} </button><br>`);
         //(click) = "search("' + products[i].categoria + '")"
-        n++;
+        // n++;
       }
     }
     this.myBrands = this.myBrands + "</div>";
@@ -61,4 +63,13 @@ export class DashboardComponent implements OnInit {
     // console.log(brands);
   }
 
+  hello(){
+    alert("hola");
+  }
+  showElements(text:string,products: Product[]){
+      for(let i=0;i<products.length;i++){
+        if(products[i].categoria==text)
+          console.log(text);
+      }
+  }
 }
